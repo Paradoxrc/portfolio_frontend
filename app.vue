de9aa6f84ae86103a0721e52597c2075f81541d3
Paradoxrc <dinithminindu02@gmail.com>
2024-05-12 19:24:43 +0530

    1st

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
 <template>
+  <h1>{{ name }}</h1>
+
+  <div v-if="pending">Loading Projects...</div>
   <div>
-    <h1>Dinith Edirisinghe</h1>
+    <h2>Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+
+  <div v-if="pending1">Loading Blogs...</div>
+  <div>
+    <h2>Blogs</h2>
+    <ul>
+      <li v-for="blog in blogs">
+        <h3>{{ blog.title }}</h3>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name = "Akith Chandinu";
+const {
+  data: projects,
+  pending,
+  error,
+} = useFetch("http://localhost:5000/projects");
+const {
+  data: blogs,
+  pending1,
+  error1,
+} = useFetch("http://localhost:5000/blogs");
+</script>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
